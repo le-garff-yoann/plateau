8fca37b5c4cdf520434e04b6c9318d85873d8ccb
le-garff-yoann <pe.weeble@yahoo.fr>
2019-07-01 16:17:06 +0200

    fix(t2pg.bash) incorrects function names)

--- t2pg.bash
+++ t2pg.bash
@@ -12,11 +12,9 @@ _t2pg_req() {
 
     local match_id=$(curl -b $COOKIE_FILE $BASE/api/matchs 2>/dev/null | jq -r '.[0]')
 
-    if [[ $match_id == "null" ]]
-    then
-        match_id=$(curl 2>/dev/null -b $COOKIE_FILE -X POST $BASE/api/matchs \
-            -d '{"number_of_players_required":2}' | jq -r .id)
-    fi
+    [[ $match_id == "null" ]] && \
+    match_id=$(curl 2>/dev/null -b $COOKIE_FILE -X POST $BASE/api/matchs \
+        -d '{"number_of_players_required":2}' | jq -r .id)
 
     curl $BASE/api/matchs/$match_id$3 -b $COOKIE_FILE ${@:3} 2>/dev/null | jq .
 }
@@ -27,32 +25,26 @@ t2pg_cleanup() {
     rm -f $1.cookie
 }
 
-t2pg_cleanup() {
-    [[ -z $1 ]] && return 1
-
-    _t_pl_req $1 "$2" ""
-}
-
 t2pg_deals() {
     [[ -z $1 ]] && return 1
 
-    _t_pl_req $1 "$2" /deals
+    _t2pg_req $1 "$2" /deals
 }
 
 t2pg_send() {
     [[ -z $1 ]] && return 1
 
-    _t_pl_req $1 "$3" / -X PATCH -d "{\"request\":\"$2\"}"
+    _t2pg_req $1 "$3" / -X PATCH -d "{\"request\":\"$2\"}"
 }
 
-tt2pg_setupmatch() {
+t2pg_setupmatch() {
     [[ -z $1 || -z $2 ]] && return 1 
 
     (
-        t_pl_send $1 PLAYER_WANT_TO_JOIN $3 && \
-        t_pl_send $2 PLAYER_WANT_TO_JOIN $3 && \
-        t_pl_send $2 PLAYER_WANT_TO_START_THE_GAME $3 && \
-        t_pl_send $2 PLAYER_ACCEPTS $3 && \
-        t_pl_send $1 PLAYER_ACCEPTS $3
+        t2pg_send $1 PLAYER_WANT_TO_JOIN $3 && \
+        t2pg_send $2 PLAYER_WANT_TO_JOIN $3 && \
+        t2pg_send $2 PLAYER_WANT_TO_START_THE_GAME $3 && \
+        t2pg_send $2 PLAYER_ACCEPTS $3 && \
+        t2pg_send $1 PLAYER_ACCEPTS $3
     ) 1>/dev/null && echo "Done"
 }
